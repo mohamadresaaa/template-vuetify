0de2c9367aefd1a928c7a4597be417d4bd260a95
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-01-19 13:41:50 +0330

    test system grid

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,29 @@
 <template>
   <div>
     <h1>home page</h1>
+    <v-container fluid class="my-5">
+      <h1>sample for note :)</h1>
+      <v-card flat>
+        <v-layout row wrap class="pa-3">
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Project title</div>
+            <div>Create a new website</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Person</div>
+            <div>The Net Ninja</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Due by</div>
+            <div>1st Jan 2019</div>
+          </v-flex>
+          <v-flex xs2 sm4 md2>
+            <div class="caption grey--text">Status</div>
+            <div>ongoing</div>
+          </v-flex>
+        </v-layout>
+      </v-card>
+
+    </v-container>
   </div>
 </template>
