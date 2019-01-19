f0b4691398de10f5eb73c28bceba01101a3b097a
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-01-19 15:25:45 +0330

    add chip to card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,27 +3,66 @@
     <h1>home page</h1>
     <v-container fluid class="my-5">
       <h1>sample for note :)</h1>
-      <v-card flat>
-        <v-layout row wrap class="pa-3">
+      <v-card flat v-for="note in notes" :key="note.notes" class="mb-1">
+        <v-layout row wrap :class="`pa-3 project ${note.status}`">
           <v-flex xs12 md6>
             <div class="caption grey--text">Project title</div>
-            <div>Create a new website</div>
+            <div>{{note.title}}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Person</div>
-            <div>The Net Ninja</div>
+            <div>{{note.person}}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Due by</div>
-            <div>1st Jan 2019</div>
+            <div>{{note.due}}</div>
           </v-flex>
           <v-flex xs2 sm4 md2>
-            <div class="caption grey--text">Status</div>
-            <div>ongoing</div>
+            <div>
+              <v-chip small :class="`${note.status} white--text caption`">{{note.status}}</v-chip>
+            </div>
           </v-flex>
         </v-layout>
+        <v-divider></v-divider>
       </v-card>
 
     </v-container>
   </div>
 </template>
+
+<style>
+  .project.complete{
+    border-left: 4px solid #2ecc71;
+  }
+  .project.ongoing{
+    border-left: 4px solid #f39c12;
+  }
+  .project.overdue{
+    border-left: 4px solid #e74c3c;
+  }
+
+  .v-chip.complete{
+    background-color: #2ecc71;
+  }
+  .v-chip.ongoing{
+    background-color: #f39c12;
+  } 
+  .v-chip.overdue{
+    background-color: #e74c3c;
+  }
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      notes: [
+        { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ]
+    }
+  }
+}
+</script>
