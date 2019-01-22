9ed41c69c53df7e73766e5983b342695d5a995a0
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-01-22 11:05:29 +0330

    add article card to blog page

--- src/views/‌Blog.vue
+++ src/views/‌Blog.vue
@@ -1,5 +1,37 @@
 <template>
   <div>
-    <h1>blog page</h1>
+    <h1 class="font-weight-medium text-capitalize">blog</h1>
+    <v-container fill-height fluid>
+      <v-layout justify-center align-center wrap>
+        <v-flex xs12>
+          <v-container grid-list-xl pa-0>
+            <v-layout row wrap>
+              <v-flex xs12 sm6 md4>
+                <v-card>
+                  <v-img class="white--text" height="200px" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
+                  </v-img>
+                  <v-card-title>
+                    <div>
+                      <span class="grey--text">Number 10</span><br>
+                      <span>Whitehaven Beach</span><br>
+                      <span>Whitsunday Island, Whitsunday Islands</span>
+                      <div>Vuetify Material Dashboard is a beautiful resource built over Vuetify, Vuex and Vuejs. It will help you get started developing dashboards in no time.</div>
+                    </div>
+                  </v-card-title>
+                  <v-card-actions>
+                    <v-btn disabled flat color="gery" class="text-capitalize">
+                    
+                    mohamadreza mosalli
+                    </v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary" class="text-capitalize">read more</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
