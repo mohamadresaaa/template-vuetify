740cc449010cf01e819b81bf98c728c0e04b6ef1
Mohamadreza Mosalli <mohamadrezamosalli@gmail.com>
2019-01-22 11:44:54 +0330

    add data

--- src/views/‌Blog.vue
+++ src/views/‌Blog.vue
@@ -6,22 +6,20 @@
         <v-flex xs12>
           <v-container grid-list-xl pa-0>
             <v-layout row wrap>
-              <v-flex xs12 sm6 md4>
+              <v-flex xs12 sm6 md4 v-for="(article, index) in articles" :key="index">
                 <v-card>
                   <v-img class="white--text" height="200px" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
                   </v-img>
                   <v-card-title>
                     <div>
-                      <span class="grey--text">Number 10</span><br>
-                      <span>Whitehaven Beach</span><br>
-                      <span>Whitsunday Island, Whitsunday Islands</span>
-                      <div>Vuetify Material Dashboard is a beautiful resource built over Vuetify, Vuex and Vuejs. It will help you get started developing dashboards in no time.</div>
+                      <span class="grey--text">{{article.due}}</span><br>
+                      <span>{{article.title}}</span>
+                      <div class="mt-3">{{article.content}}</div>
                     </div>
                   </v-card-title>
                   <v-card-actions>
                     <v-btn disabled flat color="gery" class="text-capitalize">
-                    
-                    mohamadreza mosalli
+                    {{ article.person }}
                     </v-btn>
                     <v-spacer></v-spacer>
                     <v-btn color="primary" class="text-capitalize">read more</v-btn>
@@ -35,3 +33,17 @@
     </v-container>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      articles: [
+        { title: 'Design a new website', person:'mohamadreza mosalli', due: '1st Jan 2019', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Code up the homepage', person:'saeed oraji', due: '10th Jan 2019', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design video thumbnails', person:'ali mohamadi', due: '20th Dec 2018', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a community forum', person:'the ninja', due: '20th Oct 2018', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ]
+    }
+  }
+}
+</script>
